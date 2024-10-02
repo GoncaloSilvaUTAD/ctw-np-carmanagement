e497c53992818626235c3c828f8a74990403a7b8
GoncaloSilvaUTAD <al60743@alunos.utad.pt>
2024-10-02 10:52:50 +0100

    primeiro commit

--- car-management-api/src/main/java/com/ctw/car/boundary/CarResource.java
+++ car-management-api/src/main/java/com/ctw/car/boundary/CarResource.java
@@ -3,15 +3,21 @@ package com.ctw.car.boundary;
 import com.ctw.car.control.CarService;
 import com.ctw.car.entity.Car;
 import com.ctw.car.entity.Routes;
+import io.quarkus.qute.Location;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import org.jboss.logging.Logger;
 
 import java.util.List;
 
-@Path(Routes.CAR)
+import static io.quarkus.arc.ComponentsProvider.LOG;
+
+@Path(Routes.CAR) // Routes.CAR = "car"
 @ApplicationScoped
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -24,11 +30,38 @@ public class CarResource {
         this.carService = carService;
     }
 
+    @Inject
+    Template index;
+    Template create;
+
     @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_HTML})
     public Response getCars(
-            @QueryParam("brand") String brand
+            @QueryParam("brand") String brand,
+            @HeaderParam("Accept") String acceptHeader
     ) {
         List<Car> cars = this.carService.getCars();
-        return Response.status(200).entity(cars).build();
+        if (acceptHeader != null && acceptHeader.contains(MediaType.TEXT_HTML)) {
+            TemplateInstance content = index.data("cars", cars);
+            return Response.ok(content.render()).type(MediaType.TEXT_HTML_TYPE).build();
+        } else {
+            return Response.ok(cars).type(MediaType.APPLICATION_JSON_TYPE).build();
+        }
+    }
+
+    @GET
+    @Path("/create")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance showCreate()
+    {
+        return create.instance();
+    }
+
+    @Path("/createSubmit")//maybe you don't need a path? Or just need different one?
+    @POST
+    public String createCar(Car car)
+    {
+        System.out.println("Received car: " + car);
+        return "{\"message\": \"Car created successfully\"}";
     }
 }
--- car-management-api/src/main/java/com/ctw/car/entity/Routes.java
+++ car-management-api/src/main/java/com/ctw/car/entity/Routes.java
@@ -2,4 +2,5 @@ package com.ctw.car.entity;
 
 public final class Routes {
     public static final String CAR = "car";
+
 }
